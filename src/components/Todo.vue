696845982381e1a7fa770523dc9e1c65a3eb2ad0
Abdullah Zeidan <zeid0012@algonquinlive.com>
2019-06-21 12:15:47 -0400

    Added a Form and Slots in the Form Component.

---
+++ src/components/Todo.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="(todo, index) in todos" :key="index" @click="todoDone(index)">{{ todo }}</li>
+  </ul>
+</template>
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    todoDone(index) {
+      console.log(index);
+      // $emit emits an event from the child component to the parent component
+      this.$emit("removeTodo", index);
+    }
+  }
+};
+</script>
---
+++ src/views/TodoList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>To Do List</h1>
+    <TodoForm @newTodo="addTodo">
+      <h2 slot="title">Add a To-Do</h2>
+      <p slot="desc">Your To-Dos will be saved.</p>
+    </TodoForm>
+    <Todo :todos="todoList" @removeTodo="appDeleteTodo"/>
+  </div>
+</template>
+<script>
+import Todo from "@/components/Todo.vue";
+import TodoForm from "@/components/TodoForm.vue";
+
+export default {
+  data() {
+    return {
+      todoList: ["Walk the Dog", "Go for a Ride"]
+    };
+  },
+  components: {
+    Todo,
+    TodoForm
+  },
+  methods: {
+    appDeleteTodo(index) {
+      this.todoList.splice(index, 1);
+    },
+    addTodo(todo) {
+      this.todoList.push(todo);
+    }
+  }
+};
+</script>
+<style>
+ul {
+  list-style: none;
+  width: 50%;
+  margin: 0% auto;
+}
+ul li {
+  border-bottom: 1px solid #acacac;
+  padding: 10px 0;
+  margin-bottom: 10px;
+}
+</style>
